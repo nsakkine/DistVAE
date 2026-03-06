3632570d2192530cbfe32e82822bcfc0fc756f6d
nsakkine <niko.sakkinen@amd.com>
2026-03-06 18:49:52 +0200

    Fixed typo

--- distvae/models/layers/conv3d.py
+++ distvae/models/layers/conv3d.py
@@ -213,16 +213,16 @@ class PatchConv3d(nn.Conv3d):
                         conv_res = F.conv3d(input, weight, bias, self.stride,
                                             self.padding, self.dilation, self.groups)
                         if halo_width[1] == 0:
-                            crop_slice[d] = slice(halo_width[0], None)
+                            crop_slice[patch_dim] = slice(halo_width[0], None)
                         else:
-                            crop_slice[d] = slice(halo_width[0], -halo_width[1])
+                            crop_slice[patch_dim] = slice(halo_width[0], -halo_width[1])
                         return conv_res[tuple(crop_slice)].contiguous()
                     else:
                         conv_res = F.conv3d(F.pad(input, padding, "constant", 0.0),
                                             weight, bias, self.stride,
                                             _triple(0), self.dilation, self.groups)
                         crop_slice = [slice(None), slice(None), slice(None), slice(None)]
-                        crop_slice[d] = slice(halo_width[0], halo_width[0] + patch_size)
+                        crop_slice[patch_dim] = slice(halo_width[0], halo_width[0] + patch_size)
                         return conv_res[tuple(crop_slice)].contiguous()
 
             else:
